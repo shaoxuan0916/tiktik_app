2620b9e96016590859d5c0f5c07a43bd59bdcebd
shaoxuan0916 <shaoxuanhinhua@gmail.com>
2022-10-22 19:37:55 +0800

    fixed responsive issues

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -24,7 +24,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
           <div className="h-[92vh] overflow-hidden xl:hover:overflow-auto">
             <Sidebar />
           </div>
-          <div className="mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1">
+          <div className="mt-4 ml-10 xl:ml-0 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1">
             <Component {...pageProps} />
           </div>
         </div>
